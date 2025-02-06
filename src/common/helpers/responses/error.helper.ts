6206a6e6b28c39fc3fd9bb80ed40d21138318739
gvitorlima <git624886@gmail.com>
2025-02-06 13:32:54 -0300

    feat: helper and dto from error response

--- src/common/helpers/responses/error.helper.ts
+++ src/common/helpers/responses/error.helper.ts
@@ -1,7 +1,22 @@
+import { HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { ErrorResponseDto } from 'src/common/dto/responses/error-response.dto';
 import { ZodError } from 'zod';
 
-export function formatError(error: Error | ZodError) {
-  if (error instanceof ZodError) {
-    console.log(error.errors)
+@Injectable()
+export class ErrorHelper {
+  private static readonly logger = new Logger();
+
+  static throwValidationError(error: ZodError): never {
+    throw new ErrorResponseDto(400, 'Validation Error', error.format());
+  }
+
+  static throwCustomError(
+    code: HttpStatus,
+    message: string,
+    details?: any,
+  ): never {
+    this.logger.error({ message: message, context: details });
+
+    throw new ErrorResponseDto(Number(code.toString()), message, details);
   }
 }
