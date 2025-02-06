bc758a21c97a1c0f5d5b4f375988c2139b2fa67a
gvitorlima <git624886@gmail.com>
2025-02-06 13:32:05 -0300

    format: valitaion from PORT field in .env

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod';
 
 export const configSchema = z.object({
-  PORT: z.number(),
+  PORT: z.string().regex(/(\d+){4}/, {
+    message: 'Field PORT in .env must contain only four numbers',
+  }),
   BASE_URL_API: z.string(),
 });
