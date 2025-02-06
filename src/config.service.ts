454aa11a0497ebf4c00a4e933cdcb51366f8ffe4
gvitorlima <git624886@gmail.com>
2025-02-06 17:28:20 -0300

    format: turns a config service into a singleton

--- src/config.service.ts
+++ src/config.service.ts
@@ -5,12 +5,23 @@ import { Injectable } from '@nestjs/common';
 /**
  * Classe que sobrepõe o método @method get padrão do ConfigService
  * adicionando tipagem com base no schema zod para validação do .env
- * 
+ *
  * @extends ConfigService
  */
-@Injectable()
 export class TypedConfigService extends ConfigService {
+  private static instance: TypedConfigService;
+
+  static getInstance(): TypedConfigService {
+    if (!this.instance) {
+      return new this();
+    }
+
+    return this.instance;
+  }
+
   get<T extends keyof Config>(configPath: T): Config[T] {
     return super.get<Config[T]>(configPath)!;
   }
 }
+
+export const typedConfigInstance: TypedConfigService = TypedConfigService.getInstance();
